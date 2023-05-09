1bd073aed9b4a8985219ed0441346882c37fe2d2
Andreas Brandmaier <andy@brandmaier.de>
2023-05-09 09:23:36 +0200

    predictions

---
+++ barnum.R
@@ -0,0 +1,8 @@
+output_path <- "predictions/"
+
+zodiac <- c("fruehling","sommer","herbst","winter")
+
+sapply(zodiac, function(x){
+rmarkdown::render(input="barnum.Rmd", 
+                  output_file=paste0(output_path,x,".pdf"))
+})
